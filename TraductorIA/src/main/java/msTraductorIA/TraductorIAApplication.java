9408cfba34d515976a90334e9f4cd3bcc07c3d46
Torres-Leonardo <leonardo.torres@vallegrande.edu.pe>
2024-04-15 08:54:21 -0500

    Traductor con servico cognitivo

---
+++ TraductorIA/src/main/java/msTraductorIA/TraductorIAApplication.java
@@ -0,0 +1,13 @@
+package msTraductorIA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TraductorIAApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TraductorIAApplication.class, args);
+	}
+
+}
