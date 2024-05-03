b8d50f56ca049348bc50e2c4847782222c1f11d5
francis VG <francis.castillo@vallegrande.edu.pe>
2024-05-03 13:13:16 -0500

    ACTUALIZACION: se agrego los permisos para conexiones de navegador(READ, CREATE and DELETE(fisico))

--- TraductorIA/src/main/java/pe/edu/vallegrande/cassiatec/TraductorIA_Application.java
+++ TraductorIA/src/main/java/pe/edu/vallegrande/cassiatec/TraductorIA_Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CassiatecApplication {
+public class TraductorIA_Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CassiatecApplication.class, args);
+		SpringApplication.run(TraductorIA_Application.class, args);
 	}
 
 }
