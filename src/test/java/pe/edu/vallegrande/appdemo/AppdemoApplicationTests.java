6add2e6517a30ab7a0da11dafed28a313111c971
26_TorresLeonardo <leonardo.torres@vallegrande.edu.pe>
2024-05-17 01:30:20 -0500

    Update AppdemoApplicationTests.java

--- src/test/java/pe/edu/vallegrande/appdemo/AppdemoApplicationTests.java
+++ src/test/java/pe/edu/vallegrande/appdemo/AppdemoApplicationTests.java
@@ -5,14 +5,15 @@ import org.springframework.test.context.ContextConfiguration;
 import pe.edu.vallegrande.cassiatec.model.entity.TraduccionesGuardadas;
 import pe.edu.vallegrande.cassiatec.TraductorIA_Application;
 
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
 @ContextConfiguration(classes = TraductorIA_Application.class)
 class AppdemoApplicationTests {
 
 	@Test
 	void contextLoads() {
-		TraduccionesGuardadas traduccionesGuardadas = new TraduccionesGuardadas();
-		traduccionesGuardadas.setId_traducciones_guardadas(1);
-		assertNotNull(traduccionesGuardadas.getId_traducciones_guardadas());
+	    TraduccionesGuardadas traduccionesGuardadas = new TraduccionesGuardadas();
+	    traduccionesGuardadas.setId_traducciones_guardadas(1L);
+	    assertNotNull(traduccionesGuardadas.getId_traducciones_guardadas());
 	}
-
 }
